2cc88cea4b1636558cef9a16fde6e48a1eb8d91c
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-29 17:00:33 +0600

    Now dashboard can see only loged in person

--- src/components/Pages/Shared/Header/Navbar.js
+++ src/components/Pages/Shared/Header/Navbar.js
@@ -1,9 +1,25 @@
 import { faMotorcycle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../Firebase.init';
 
 const Navbar = () => {
+    const [user] = useAuthState(auth)
+    const logout = () => {
+        signOut(auth);
+
+    };
+    const menuItem = <>
+        <li><Link to='/home'>Home</Link></li>
+        <li><Link to='/purchase'>Purchase</Link></li>
+        {
+            user && <li><Link to='/dashboard'>Dashboard</Link></li>
+
+        }        <li><Link to='/About'>About</Link></li>
+        <li><Link to='/contact'>Contact Us</Link></li></>
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -13,36 +29,18 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/home'>Home</Link></li>
-                            <li><Link to='/about'>About</Link></li>
-                            <li><Link to='/appointment'>Appoinment</Link></li>
-                            <li><Link to='/reviews'>Reviews</Link></li>
-                            <li><Link to='/contactus'>Contact Us</Link></li>
-                            <li><Link to='/login'>Login</Link></li>
+                            {menuItem}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost  normal-case text-xl"> <FontAwesomeIcon className='text-xl mx-2' icon={faMotorcycle}></FontAwesomeIcon>
-                        Parts Factory <span className='text-primary'> BD.</span></a>
+                    <Link to='/' className="btn btn-ghost  normal-case text-xl"> <FontAwesomeIcon className='text-xl mx-2' icon={faMotorcycle}></FontAwesomeIcon>
+                        Parts Factory <span className='text-primary'> BD.</span></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><Link to='/home'>Home</Link></li>
-                        <li><Link to='/purchase'>Purchase</Link></li>
-                        <li><Link to='/dashboard'>Dashboard</Link></li>
-                        <li><Link to='/About'>About</Link></li>
-
-                        <li tabindex="0">
-                            <a>
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><Link to='/contact'>Contact Us</Link></li>
-                        <li><Link to='/login'>Login</Link></li>
+                        {menuItem}
+                        {
+                            user ? <button onClick={logout} className='btn btn-ghost'>Sign Out</button> : <Link className='btn btn-ghost' to='/login'>Login</Link>
+                        }
                     </ul>
                 </div>
             </div>
