ad78fa8779e998b0fff6453af3b44b036765a8f3
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-30 23:16:01 +0600

    Add profile info

--- src/components/Pages/Dashboard/MyProfile.js
+++ src/components/Pages/Dashboard/MyProfile.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase.init';
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
+    console.log(user);
     return (
         <div>
-
+            <div class="card mx-auto w-96 bg-base-100 shadow-xl">
+                <figure class="px-10 pt-10">
+                    <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" alt="Shoes" class="rounded-xl" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">Name: {user.displayName}</h2>
+                    <p>Email: {user.email}</p>
+                    <div class="card-actions">
+                        <button class="btn btn-primary">Update Profile</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
